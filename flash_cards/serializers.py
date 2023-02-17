59802a8ef8dfc2db0222d2160e404366ca78e37c
jallen2787 <jallen221924@gmail.com>
2023-02-17 17:22:31 -0500

    fixed spelling error on models to model

--- flash_cards/serializers.py
+++ flash_cards/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
 from .models import FlashCard
 
-class FlashCardSerializer(serializers.ModelsSerializer):
+class FlashCardSerializer(serializers.ModelSerializer):
     class Meta: 
         model = FlashCard
         fields = '__all__'
 
-        
